d68f0911042c9e26f1871fd83b4caf845c342bb4
Norton dos Santos <nortonsantos79@gmail.com>
2021-07-27 15:51:55 -0300

    setei o engine para o handlebars e o layout padrao como main

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 
+//view engine
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+
 app.listen(8080);
