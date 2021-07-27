f87e03eb20648e701f42db1f232c3f86a6900390
Norton dos Santos <nortonsantos79@gmail.com>
2021-07-27 16:06:34 -0300

    setei a rota default para renderizar o index.handlebars

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const handlebars = require("express-handlebars");
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//routes
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.listen(8080);
