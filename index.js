fb2a54daccbf7a60e10a7c3dcb7c6b5925052214
Norton dos Santos <nortonsantos79@gmail.com>
2021-07-27 15:54:39 -0300

    adicionei view engine handlebars no app.set

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const handlebars = require("express-handlebars");
 
 //view engine
-
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.listen(8080);
