f0d0bc722a80ffac348880c44f9eab56d27d52c1
Norton dos Santos <nortonsantos79@gmail.com>
2021-07-28 16:16:13 -0300

    criada a conexão com o mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 
+const mongoose = require("mongoose");
 const multer = require("./multer.js");
 const filehelper = require("./file-helper.js");
 //view engine
@@ -11,6 +12,18 @@ app.set('view engine', 'handlebars')
 //public
 const path = require("path");
 app.use(express.static(path.join(__dirname, "/public")));
+
+//db
+
+mongoose.connect('mongodb://localhost/pets', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Conexão realizada com sucesso!");
+}).catch((err) => {
+    console.log(err);
+});
+
 
 //routes
 app.get("/", (req, res) => {
